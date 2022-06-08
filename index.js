b2e1c401722e16404d08128d034836e2dbccd857
Kipleting <kipletingedwin4@gmail.com>
2022-06-08 18:15:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+//declaring variables
+var customerName='bob';
+const nameOfCustomer='name';
+const leastFavoriteCustomer='initial value';
+
+//return an Uppercase name
+
+function upperCaseCustomerName(){
+    customerName=customerName.toUpperCase()
+}
+//setting up best customer
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+//overwrites best customer
+function overwriteBestCustomer(){
+    bestCustomer ='maybe bob'
+}
+ function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer=leastFavoriteCustomer.toUpperCase
+ }  
+
+
